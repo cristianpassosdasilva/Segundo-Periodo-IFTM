9ddf25556eb31a9fa16cc3802ee80e05b8b86b2c
cristianpassosdasilva <hudson.beijo@estudante.iftm.edu.br)>
2025-10-14 21:22:59 -0300

    Terceira Etapa, retirando a validacao da classe Reta e criando uma nova classe de Validacao

--- POO/src/GeometriaRetas/App.java
+++ POO/src/GeometriaRetas/App.java
@@ -7,32 +7,49 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String resposta;
+        int totalRetas = 0;
         
         do {
             // 1 - ler as coordenadas
             System.out.println("Digite as coordenadas da reta:");
             
             System.out.print("X1: ");
-            double x1 = leCoordenada(1);
+            double x1 = leCoordenada(scanner);
             
             System.out.print("Y1: ");
-            double y1 = leCoordenada(2);
+            double y1 = leCoordenada(scanner);
             
             System.out.print("X2: ");
-            double x2 = leCoordenada(3);
+            double x2 = leCoordenada(scanner);
             
             System.out.print("Y2: ");
-            double y2 = leCoordenada(4);
+            double y2 = leCoordenada(scanner);
             
-            // 2.1 - instanciar a classe Retas
-            Retas reta = new Retas(x1, y1, x2, y2);
+            // 2 - se coordenadas forem válidas (usando classe Validacao)
+            // Demonstrando uso dos dois métodos da classe Validacao
+            boolean coordenadasValidas = Validacao.valida(x1, y1, x2, y2);
+            boolean noQuadranteUm = Validacao.isQuadOne(x1, y1, x2, y2);
             
-            // 2 - se coordenadas forem válidas (usando método da classe Retas)
-            if (reta.valida(x1, y1, x2, y2)) {
+            System.out.println("Validação básica: " + coordenadasValidas);
+            System.out.println("Está no 1º quadrante: " + noQuadranteUm);
+            
+            if (coordenadasValidas && noQuadranteUm) {
+                // 2.1 - instanciar a classe Retas
+                Retas reta = new Retas(x1, y1, x2, y2);
+                
                 // 2.2 - calcular o comprimento
-                // 2.3 - contar retas  
+                // 2.3 - contar retas (usando o cont público)
+                totalRetas += reta.cont;
+                
+                // Demonstrar o uso do cont público
+                System.out.println("Esta é a reta número: " + reta.cont);
+                
                 // 2.4 - exibir
                 System.out.println(reta.exibe());
+                
+                // Exemplo: incrementar o contador após exibir
+                reta.cont++;
+                System.out.println("Contador após incremento: " + reta.cont);
                 
             } else {
                 System.out.println("ERRO: As coordenadas devem estar no 1º quadrante!");
@@ -44,11 +61,11 @@ public class App {
             
         } while (resposta.equalsIgnoreCase("s"));
         
+        System.out.println("\nTotal de retas válidas criadas: " + totalRetas);
         scanner.close();
     }
     
-    public static double leCoordenada(int posicao) {
-        Scanner scanner = new Scanner(System.in);
+    public static double leCoordenada(Scanner scanner) {
         return scanner.nextDouble();
     }
 }
---
+++ POO/src/GeometriaRetas/Validacao.java
@@ -0,0 +1,12 @@
+package GeometriaRetas;
+
+public class Validacao {
+    
+    public static boolean valida(double x1, double y1, double x2, double y2) {
+        return x1 > 0 && y1 > 0 && x2 > 0 && y2 > 0;
+    }
+    
+    public static boolean isQuadOne(double x1, double y1, double x2, double y2) {
+        return x1 > 0 && y1 > 0 && x2 > 0 && y2 > 0;
+    }
+}
